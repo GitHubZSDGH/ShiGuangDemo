448d218fe64544388279c274b55a048f569c1fe0
JiangXianKaiNan <774623234@qq.com>
2017-06-19 08:25:15 +0800

    修改了HttpFragctory 工类

--- app/src/main/java/test/jiyun/com/shiguangdemo/modle/https/HttpFactory.java
+++ app/src/main/java/test/jiyun/com/shiguangdemo/modle/https/HttpFactory.java
@@ -3,20 +3,24 @@ package test.jiyun.com.shiguangdemo.modle.https;
 
 public class HttpFactory {
 
-//    public static final int OKHTTP = 1;
-//    public static final int RETROFIT = 2;
+    private static final int OKHTTP = 0;
+    private static final int VOLLEY = 1;
+    private static final int RETROFIT = 2;
+    private static final int TYPE = RETROFIT;
 
 
     public static IHttp getFactory() {
-        IHttp iuSer = Retrofitimple.getInstance();
-//        switch (Inter) {
-//            case OKHTTP:
-//                iuSer = OkHttpUtils.getOkHttp();
-//                return iuSer;
-//            case RETROFIT:
-//                iuSer = Retrofitimple.getInstance();
-//                return iuSer;
-//        }
-        return iuSer;
+        IHttp http = null;
+        switch (TYPE) {
+            case OKHTTP:
+//                http = Retrofitimple.getInstance();
+                break;
+            case VOLLEY:
+                break;
+            case RETROFIT:
+                http = Retrofitimple.getInstance();
+                break;
+        }
+        return http;
     }
 }
