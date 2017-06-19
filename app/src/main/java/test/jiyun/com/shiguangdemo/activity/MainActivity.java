5c0d97cab18509a12e90b5d6593116c8c85cae65
JiangXianKaiNan <774623234@qq.com>
2017-06-19 21:30:00 +0800

    添加主布局和二级布局

--- app/src/main/java/test/jiyun/com/shiguangdemo/activity/MainActivity.java
+++ app/src/main/java/test/jiyun/com/shiguangdemo/activity/MainActivity.java
@@ -1,16 +1,76 @@
 package test.jiyun.com.shiguangdemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import test.jiyun.com.shiguangdemo.R;
+import test.jiyun.com.shiguangdemo.base.BaseActivity;
+import test.jiyun.com.shiguangdemo.config.FragmentBuilder;
+import test.jiyun.com.shiguangdemo.fragment.home.Home_Fg_View;
 
-public class MainActivity extends AppCompatActivity {
+
+/**
+ *  主布局
+ */
+public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.Main_FrameLayout)
+    FrameLayout MainFrameLayout;
+    @Bind(R.id.home_Rbt)
+    RadioButton homeRbt;
+    @Bind(R.id.ticket_Rbt)
+    RadioButton ticketRbt;
+    @Bind(R.id.mall_Rbt1)
+    RadioButton mallRbt1;
+    @Bind(R.id.live_Rbt)
+    RadioButton liveRbt;
+    @Bind(R.id.mine_Rbt)
+    RadioButton mineRbt;
+    @Bind(R.id.Main_RadioGrop)
+    RadioGroup MainRadioGrop;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutViwe() {
+        return R.layout.activity_main;
     }
 
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initloadData() {
+        FragmentBuilder.getInstance().start(Home_Fg_View.class,R.id.Main_FrameLayout).builder();
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+
+
+    @OnClick({R.id.home_Rbt, R.id.ticket_Rbt, R.id.mall_Rbt1, R.id.live_Rbt, R.id.mine_Rbt})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.home_Rbt: //首页
+                FragmentBuilder.getInstance().start(Home_Fg_View.class,R.id.Main_FrameLayout).builder();
+                break;
+            case R.id.ticket_Rbt://购票
+                break;
+            case R.id.mall_Rbt1://商城
+                break;
+            case R.id.live_Rbt://直播
+                break;
+            case R.id.mine_Rbt://我的
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/test/jiyun/com/shiguangdemo/fragment/home/Home_Fg_View.java
@@ -0,0 +1,65 @@
+package test.jiyun.com.shiguangdemo.fragment.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import test.jiyun.com.shiguangdemo.R;
+import test.jiyun.com.shiguangdemo.base.BaseFragment;
+
+/**
+ * 项目名称:时光网
+ * 类描述: 首页标题和轮播图、容器
+ * 创建人:LENOVO
+ * 创建时间:2017-6-19 20:43
+ * 修改人:
+ * 修改内容:
+ */
+
+public class Home_Fg_View extends BaseFragment {
+    @Bind(R.id.Home_fg_title_city)
+    TextView HomeFgTitleCity;
+    @Bind(R.id.Home_fg_title_SaoYiSao)
+    ImageView HomeFgTitleSaoYiSao;
+    @Bind(R.id.Home_fg_title_Search)
+    ImageView HomeFgTitleSearch;
+
+    @Override
+    protected int getLayoutViwe() {
+        return R.layout.home_fg_view;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initloadData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+
+    @OnClick({R.id.Home_fg_title_city, R.id.Home_fg_title_SaoYiSao, R.id.Home_fg_title_Search})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.Home_fg_title_city:// 选择城市
+                break;
+            case R.id.Home_fg_title_SaoYiSao:// 扫一扫
+                break;
+            case R.id.Home_fg_title_Search:// 搜索
+                break;
+        }
+    }
+}
